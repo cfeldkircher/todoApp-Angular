9d9010f9a1e3c72f82d32b6feca53e65fb67941c
carl <cfeldkircher2@gmail.com>
2019-11-14 14:17:19 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Response } from '@angular/http';
+import { TodoService } from './services/todo.service';
+import ToDo from './models/todo.model';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+ constructor(
+    //Private todoservice will be injected into the component by Angular Dependency Injector
+    private todoService: TodoService
+  ) { }
+
+  //Declaring the new todo Object and initilizing it
+  public newTodo: ToDo = new ToDo()
+
+  //An Empty list for the visible todo list
+  todosList: ToDo[];
+
+
+  ngOnInit(): void {
+
+    //At component initialization the 
+    this.todoService.getToDos()
+      .subscribe(todos => {
+        //assign the todolist property to the proper http response
+        this.todosList = todos
+        alert("create")
+        console.log(todos)
+      })
+  }
+   create() {
+    this.todoService.createTodo(this.newTodo)
+      .subscribe((res) => {
+        this.todosList.push(res.data)
+        this.newTodo = new ToDo()
+      })
+  }
 }
